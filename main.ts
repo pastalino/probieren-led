8bc43c6400afe1b4d038e0cdd7d3e351f3726ced
pastalino <71946107+pastalino@users.noreply.github.com>
2021-04-18 21:19:38 +0200

    init

--- main.ts
+++ main.ts
@@ -1,15 +1,49 @@
 input.onGesture(Gesture.TiltRight, function () {
-	
+    if (x >= -1 && x <= 5) {
+        basic.clearScreen()
+    }
+    x += 1
+    if (x >= 0 && x <= 4) {
+        led.plot(x, y)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
 })
 input.onGesture(Gesture.LogoUp, function () {
-	
+    if (y >= -1 && y <= 5) {
+        basic.clearScreen()
+    }
+    y += 1
+    if (y >= 0 && y <= 4) {
+        led.plot(x, y)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
 })
 input.onGesture(Gesture.TiltLeft, function () {
-	
+    if (x >= -1 && x <= 5) {
+        basic.clearScreen()
+    }
+    x += -1
+    if (x >= 0 && x <= 4) {
+        led.plot(x, y)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
 })
 input.onGesture(Gesture.LogoDown, function () {
-	
+    if (y >= -1 && y <= 5) {
+        basic.clearScreen()
+    }
+    y += -1
+    if (y >= 0 && y <= 4) {
+        led.plot(x, y)
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
 })
-let x = 0
 let y = 0
+let x = 0
+x = 0
+y = 0
 led.plot(0, 0)
