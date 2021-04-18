49df79dac6a247a4a9d363635f795f641f3dcad2
pastalino <71946107+pastalino@users.noreply.github.com>
2021-04-18 20:06:23 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.py
@@ -0,0 +1,15 @@
+def on_gesture_tilt_right():
+    pass
+input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
+
+def on_gesture_logo_up():
+    pass
+input.on_gesture(Gesture.LOGO_UP, on_gesture_logo_up)
+
+def on_gesture_tilt_left():
+    pass
+input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
+
+def on_gesture_logo_down():
+    pass
+input.on_gesture(Gesture.LOGO_DOWN, on_gesture_logo_down)
---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onGesture(Gesture.TiltRight, function () {
+	
+})
+input.onGesture(Gesture.LogoUp, function () {
+	
+})
+input.onGesture(Gesture.TiltLeft, function () {
+	
+})
+input.onGesture(Gesture.LogoDown, function () {
+	
+})
+let x = 0
+let y = 0
+led.plot(0, 0)
